b116cac03ed93da71421ce42e486e6c2ae2a093d
Heinrich <dortonbrandheinrich@gmail.com>
2025-07-21 14:05:19 +1200

    Add files via upload

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.config(t)
+    t.console = true
+    t.window.title = "Game Name"
+
+    -- Using 1080p (for now)
+    t.window.width = 1280 
+    t.window.height = 720
+
+    t.window.resizable = true
+end
---
+++ main.lua
@@ -0,0 +1,14 @@
+local enemy = require "Enemy"
+local wf = require "libs.windfield"
+
+function love.load ()
+    
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+    
+end
