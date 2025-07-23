8ae975d296dc20cd8aa3410438e7b8700d809e82
bushbush1 <dortonbrandheinrich@gmail.com>
2025-07-23 19:47:45 +1200

    working on sprite animation,

--- devnotes/DailyNotes.md
+++ devnotes/DailyNotes.md
@@ -5,10 +5,10 @@ Day one (20/7/25):
 1. did some basic planing and setting up as much of my dev environment as i could.
 2. decided on the basic features that I am planning on implementing for a minimal viable game. (MVG)
 3. spent about 1-2 hours working on it
-**total hours = 1 and 1/2**
+**total hours = 1hour 30 mins**
 
 Day two (21/7/25):
-1. **time spent working = 1-2.30 = 1 and 1/2**
+1. **time spent working = 1-2.30 = 1hour 30 mins**
 2. **what did i do**:
 - did some reasearch into enemies and their movement etc as i have never worked with this before. 
 - watched a couple videos online. Did some research into running the game from the terminal so i dont have to drag and drop the main file onto the exc everytime i want to run the game. 
@@ -16,9 +16,18 @@ Day two (21/7/25):
 3. 
 
 Day Three (22/07/25)
-1. **Time spent working (9am-10am, 1.30pm - 2.45, 4pm -)**
+1. **Time spent working (9am-10am, 1.30pm - 2.45, 4pm -6 (4hrs 15mins))**
 2. **what did i do** - did some basic research into tiled and map creation. watched a video on this. tried to create a test map using tiled and importing it into vs code to use as a map. currently the sizing is a bit weird but will try figure that out later in the day or another day. tried some time finding sprites and then attempting to create the animations for these 
 3. **what didnt work/struggled with** - today i struggled with the animation of the character sprites. the sheet the tutorial used had all directions on one sprit sheet where i have multiple sheets. i manged to make some progress on this issue, by flipping the sprite but it still looks super jenk....
+4. **notes for tomorrow - re watch the animation video, maybe make a bigger map (maybe rewatch map vid), and potentially slow down the player speed. potentiallly use a different sprite sheet the has multi direction assests to make animation appear smoother. add git ignore to game structure if you didnt yesterday. re watch the camera vid and try fix the zoom in stuff. half way through camera centering vid**
 
+Day Four (23/7/25)
+1. **Time spent working (10.30-12, 4pm-5.15 )**
+**what did i do** - did some research into procedural map generation with love2d and tiled - decided agaisnt this for now as it is way above my knowledge. decided to stick with creating maps manually for now at a smaller scale.
+played around with different sprites and tried to get animations for movement working.
+2. **what didnt work/struggled with** - today i continued to struggle with animating my charater sprite.
+3. **what i learnt** - i learnt i should really read and listen more carefully as the sprite sheet dimensions for the sheet i was using were on the website the whole time...
 
-Total time spent = 3 hours
+4. **notes for tomorrow** - SET UP GIT IGNORE AND START MAKING BRANCHES, CONTINUE TO WORK ON SPRTE ANIMATION 
+
+**Total time spent = 8 hours~**
--- main.lua
+++ main.lua
@@ -1,75 +1,106 @@
 local enemy = require "Enemy"
 local wf = require "libs.windfield"
 local love = require "love"
-local worldGravity = 0;
--- player
+
 
 function love.load ()
     anim8 = require 'libs.anim8'
     love.graphics.setDefaultFilter("nearest", "nearest") -- scales pixles to avoid bluring
 
-    -- camera = require 'libs.camera'
-    -- cam1 = camera()
-    -- cam1:zoom(2)
+    camera = require 'libs.camera'
+    cam = camera()
+    cam:zoom(1)
 
     sti = require 'libs/sti'
     starterMap = sti('maps/testMap.lua')
+    biggerMap = sti('maps/testMap1.lua')
 
-    player1 = {}
-    player1.x = 25
-    player1.y = 15
-    player1.speed = 3
-    -- player1.sprite = love.graphics.newImage('sprites/knight/with_outline/IDLE.png')
-    player1.spriteSheet = love.graphics.newImage('sprites/knight/with_outline/WALK.png')
-    player1.grid = anim8.newGrid(64,64, player1.spriteSheet:getWidth(), player1.spriteSheet:getHeight()) -- splitting sprite sheet into seperate images 5 mins into the anim8 vid
+    player = {}
+    player.x = 25
+    player.y = 15
+    player.speed = .5
+    player.spriteSheet = love.graphics.newImage('sprites/multiKnight/Spritesheet_with_Shadows.png')
+    player.grid = anim8.newGrid(48,48, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
 
-    player1.animations = {}
-    player1.animations.left = anim8.newAnimation(player1.grid('1-8',1), 0.2):flipH()
-    player1.animations.right = anim8.newAnimation(player1.grid('1-8', 1), 0.2)
-    player1.animations.up = anim8.newAnimation(player1.grid('1-1', 1), 1)    -- just frame 1, slow update
-    player1.animations.down = anim8.newAnimation(player1.grid('1-1', 1), 1)  -- same frame
+
+    player.animations = {}
+    player.animations.left = anim8.newAnimation(player.grid('1-6',4), 0.2) -- left animation (first numbers is collums (how many sprites for said animation), second number is which row)
+    player.animations.right = anim8.newAnimation(player.grid('1-6', 3), 0.2) -- right animation
+    -- player.animations.up = anim8.newAnimation(player.grid('1-8', 1), 1)    -- up animation
+    -- player.animations.down = anim8.newAnimation(player.grid('1-8', 1), 1)  -- down animation
+
+    -- player.spriteSheet = love.graphics.newImage('sprites/knight/with_outline/WALK.png')
+    -- player.grid = anim8.newGrid(64,64, player.spriteSheet:getWidth(), player.spriteSheet:getHeight()) -- splitting sprite sheet into seperate images 5 mins into the anim8 vid
+    -- player.animations = {}
+    -- player.animations.left = anim8.newAnimation(player.grid('1-8',1), 0.15):flipH()
+    -- player.animations.right = anim8.newAnimation(player.grid('1-8', 1), 0.15)
+    -- player.animations.up = anim8.newAnimation(player.grid('1-8', 1), 1)  -- just frame 1, slow update
+    -- player.animations.down = anim8.newAnimation(player.grid('1-8', 1), 1) -- same frame
     
-    player1.anim = player1.animations.right
+    player.anim = player.animations.left
 end
 
 function love.update(dt)  -- runs every frame dt = delta time
     local playerMoving = false
 
     if love.keyboard.isDown("left") then
-        player1.x = player1.x - player1.speed
-        player1.anim = player1.animations.left
+        player.x = player.x - player.speed
+        player.anim = player.animations.left
         playerMoving = true
     end
     if love.keyboard.isDown("right") then
-        player1.x = player1.x + player1.speed
-        player1.anim = player1.animations.right
+        player.x = player.x + player.speed
+        player.anim = player.animations.right
         playerMoving = true
     end
      if love.keyboard.isDown("up") then
-        player1.y = player1.y  - player1.speed
-        player1.anim = player1.animations.up
+        player.y = player.y  - player.speed
+        -- player.anim = player.animations.up
         playerMoving = true
     end
     if love.keyboard.isDown("down") then
-        player1.y = player1.y + player1.speed
-        player1.anim = player1.animations.down
+        player.y = player.y + player.speed
+        -- player.anim = player.animations.down
         playerMoving = true
     end
 
     -- if the character isnt move set it to frame 1 
     if playerMoving == false then
-        player1.anim:gotoFrame(1)
+        player.anim:gotoFrame(1)
     end
 
-    player1.anim:update(dt)
-    
+    player.anim:update(dt)
+
+    cam:lookAt(player.x, player.y)
+
+    local w = love.graphics.getWidth()
+    local h = love.graphics.getHeight()
+
+    if cam.x < w/2 then
+        cam.x = w/2
+    end
+    if cam.y < h/2 then
+        cam.y = h/2
+    end
+
+    local mapW = biggerMap.width * biggerMap.tilewidth
+    local mapH = biggerMap.height * biggerMap.tileheight
+
+    if cam.x > (mapW - w/2) then
+        cam.x = (mapW - w/2)
+    end
+     if cam.y > (mapH - h/2) then
+        cam.y = (mapH - h/2)
+    end
 end
 
 function love.draw()
-    --cam1:attach()
+    cam:attach()
      -- drawring the tile map in different levels
-        starterMap:drawLayer(starterMap.layers["behind"])
-        starterMap:drawLayer(starterMap.layers["Top"]) 
-        player1.anim:draw(player1.spriteSheet, player1.x, player1.y, nil)
-    --cam1:detach()
+        biggerMap:drawLayer(biggerMap.layers["behind"])
+        biggerMap:drawLayer(biggerMap.layers["Top"])
+        player.anim:draw(player.spriteSheet, player.x, player.y, nil, 2,2,32,32)
+    cam:detach()
+
+    love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 10)
 end
