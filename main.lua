9deecc015c4975fa214ebaab3d1e54e4caae270d
bushbush1 <dortonbrandheinrich@gmail.com>
2025-07-29 12:09:36 +1200

    attempting to add player collider

--- main.lua
+++ main.lua
@@ -1,24 +1,28 @@
 local enemy = require "Enemy"
-local wf = require "libs.windfield"
 local love = require "love"
 
 
 function love.load ()
+    wf = require "libs.windfield"
+    world = wf.newWorld(0,0)
+
     anim8 = require 'libs.anim8'
     love.graphics.setDefaultFilter("nearest", "nearest") -- scales pixles to avoid bluring
 
     camera = require 'libs.camera'
     cam = camera()
-    cam:zoom(4)
+    cam:zoom(1)
 
     sti = require 'libs/sti'
     starterMap = sti('maps/testMap.lua')
     biggerMap = sti('maps/testMap1.lua')
 
     player = {}
+    player.collider = world:newBSGRectangleCollider(25,15, 15,30,14)
+    player.collider:setFixedRotation(true)
     player.x = 25
     player.y = 15
-    player.speed = .5
+    player.speed = 300
     player.spriteSheet = love.graphics.newImage('sprites/multiKnight/Spritesheet_with_Shadows.png')
     player.grid = anim8.newGrid(48,48, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
 
@@ -34,32 +38,40 @@ end
 
 function love.update(dt)  -- runs every frame dt = delta time
     local playerMoving = false
+    local vx = 0
+    local vy = 0
 
     if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
-        player.x = player.x - player.speed
+        vx = player.speed * -1
         player.anim = player.animations.left
         playerMoving = true
     end
     if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
-        player.x = player.x + player.speed
+        vx = player.speed 
         player.anim = player.animations.right
         playerMoving = true
     end
      if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
-        player.y = player.y  - player.speed
+        vy = player.speed * -1
         player.anim = player.animations.up
         playerMoving = true
     end
     if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
-        player.y = player.y + player.speed
+        vy = player.speed
         player.anim = player.animations.down
         playerMoving = true
     end
+
+    player.collider:setLinearVelocity(vx,vy)
 
     -- if the character isnt move set it to frame 1 
     if playerMoving == false then
         player.anim:gotoFrame(1)
     end
+
+    world:update(dt)
+    player.x = player.collider:getX()
+    player.y = player.collider:getY()
 
     player.anim:update(dt)
 
@@ -92,6 +104,7 @@ function love.draw()
         biggerMap:drawLayer(biggerMap.layers["behind"])
         biggerMap:drawLayer(biggerMap.layers["Top"])
         player.anim:draw(player.spriteSheet, player.x, player.y, nil, 2,2,32,32)
+        world:draw()
     cam:detach()
 
     love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 10)
